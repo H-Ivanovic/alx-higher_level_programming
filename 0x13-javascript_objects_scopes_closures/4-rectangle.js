d02e5bba3eafcc5e39c985d46c435ef412a1935a
H-Ivanovic <hamalaivan808@gmail.com>
2023-03-14 20:51:31 +0000

    obj

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,6 +1,5 @@
-
-
-rts = class Rectangle {
+#!/usr/bin/node
+module.exports = class Rectangle {
 	  constructor (w, h) {
 		      if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
 		    }
@@ -9,11 +8,11 @@ rts = class Rectangle {
 		      for (let i = 0; i < this.height; i++) console.log('X'.repeat(this.width));
 		    }
 
-	  double () {
-		      [this.width, this.height] = [this.width * 2, this.height * 2];
-		    }
-
 	  rotate () {
 		      [this.width, this.height] = [this.height, this.width];
+		    }
+
+	  double () {
+		      [this.width, this.height] = [this.width * 2, this.height * 2];
 		    }
 };
--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -2,5 +2,5 @@
 module.exports = class Square extends require('./4-rectangle.js') {
 	  constructor (size) {
 		      super(size, size);
-		    }
+	  }
 };
